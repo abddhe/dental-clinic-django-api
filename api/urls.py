590fca239ddcca1b455250dad3d26feb87a98d82
Abdallah Dheir <tareqm577@gmail.com>
2023-01-09 18:26:42 +0000

    add modals

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,11 @@
-from django.urls import path,include
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import UserViewSet,DoctorViewSet,PatientViewSet
+from .views import UserViewSet, DoctorViewSet, PatientViewSet
+
 router = DefaultRouter()
 router.register(r"users", UserViewSet)
 router.register(r"doctors", DoctorViewSet)
 router.register(r"patients", PatientViewSet)
 urlpatterns = [
-path("", include(router.urls))
+    path("", include(router.urls))
 ]
--- api/views.py
+++ api/views.py
@@ -1,18 +1,22 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from .serializers import UserSerializer,DoctorSerializer,PatientSerializer
+from .serializers import UserSerializer, DoctorSerializer, PatientSerializer
 from django.contrib.auth.models import User
 from doctors.models import Doctor
 from patients.models import Patient
+
+
 # Create your views here.
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    
+
+
 class DoctorViewSet(viewsets.ModelViewSet):
     queryset = Doctor.objects.all()
     serializer_class = DoctorSerializer
+
 
 class PatientViewSet(viewsets.ModelViewSet):
     queryset = Patient.objects.all()
